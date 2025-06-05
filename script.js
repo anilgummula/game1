f2d89dcd83198fc2c7dcb57e014f8fefa877d137
anil gummula <anilgummula2060@gmail.com>
2025-06-05 11:17:52 +0530

    05-06-25 updated

--- script.js
+++ script.js
@@ -1,18 +1,57 @@
 let score = 0;
+// let value=20;
+let highScore = 0;
+let timer = 20;
+let timerInterval;
+let moveInterval;
+let drag = 0;
+
+function startGame() {
+
+    score = 0;
+    timer = 20;
+    document.getElementById("score").innerText = `Score : ${score}`;
+    document.getElementById("timer").innerText = `Timer : ${timer}s`;
+
+    document.getElementById("btn").style.display = "inline-block";
+    document.getElementById("startBtn").style.display = "none";
+
+    timerInterval = setInterval(() => {
+        if (timer > 0) {
+            document.getElementById("timer").innerText = `Timer : ${timer--}s`;
+        } else {
+            end();
+        }
+    }, 1000);
+
+    moveInterval = setInterval(() => {
+        // drag+=500;
+        let num = Math.floor(Math.random() * (1000 - 100) + 100);
+        document.getElementById("btn").style.margin = `200px ${num}px ${num}px ${num}px`;
+    }, 2000);
+}
+
 function clickme(){
 
+    
     score++;
-
+    document.getElementById("score").innerText = `Score : ${score}`;
     changeImg();
 
-    setInterval(() => {     
-        let num = Math.floor(Math.random() * (1000 - 100) + 100);
     
-        document.getElementById("btn").style.margin=`200px ${num}px ${num}px ${num}px`;
-    },4000);
-
-
-    document.getElementById("score").innerHTML=`score : ${score}`;
+    // setInterval(()=>{
+        
+        //     document.getElementById("timer").innerHTML=`Timer : ${value}`
+        //     value--;
+        // },1000)
+        
+        // setInterval(() => {     
+        //     let num = Math.floor(Math.random() * (1000 - 100) + 100);
+            
+        //     document.getElementById("btn").style.margin=`200px ${num}px ${num}px ${num}px`;
+        // },4000);
+        
+        
 }
 
 
@@ -21,4 +60,21 @@ function changeImg(){
     let type = num<9? "jpeg":"jpg";
     
     document.getElementById("img").src=`${num}.${type}`;
+}
+
+function end() {
+
+    clearInterval(timerInterval);
+    clearInterval(moveInterval);
+
+    document.getElementById("btn").style.display = "none";
+    document.getElementById("startBtn").style.display = "flex";
+    timer=20;
+
+    if (score > highScore) {
+        highScore = score;
+        document.getElementById("highscore").innerText = highScore;
+    }
+
+    alert(`Time's up! Your score is ${score}. Click OK to restart.`);
 }
